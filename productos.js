bbf15059160c499c4efc3e6b7dfca93d65701538
PaMiCasa <businessmatienzo@gmail.com>
2025-04-19 18:55:06 -0400

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,13 @@
+// productos.js
+
+const productos = [
+  {
+    id: 1,
+    nombre: "Televisor de 32 pulgadas Espectre",
+    precio: 85,
+    descripcion: "Smart tv de 32 pulgadas marca espectre.",
+    imagen: "img/televisor.png",
+    provincia: "Artemisa"
+  }
+  // Puedes agregar más productos aquí
+];
