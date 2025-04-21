77adc478c150995d96ef69fadf12338194566578
PaMiCasa <businessmatienzo@gmail.com>
2025-04-20 23:20:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ function toggleMenu() {
   menu.classList.toggle('active');
   overlay.classList.toggle('active');
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const params = new URLSearchParams(window.location.search);
+  const ref = params.get("ref");
+
+  if (ref) {
+    localStorage.setItem("ref", ref); // Guarda el identificador del gestor
+  }
+});
