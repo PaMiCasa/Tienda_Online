1794acbaedf7dbab272ebded7acdab8dfb0c702b
PaMiCasa <businessmatienzo@gmail.com>
2025-04-20 23:38:02 -0400

    Add files via upload

---
+++ ref-manager.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const ref = urlParams.get("ref");
+  if (ref) {
+    localStorage.setItem("gestorRef", ref);
+  }
+});
